e71c11dd0b97cf6d810e09c5b58e03c0ffbffd09
kelldani <kelldani100@gmail.com>
2023-11-17 12:03:43 +0100

    sorting

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+#include <atdio.h>
+
+/**
+ * bubble_sort ->> Sorts an array of int in ascending the order.
+ * @array: int array.
+ * @size: matrix size.
+ **/
+
+void bubble_sort(int *array, size_t size);
+{
+    
+}
