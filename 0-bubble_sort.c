bf0ae3092a75fd62ca83a1368aad5d228ec5736b
kelldani <kelldani100@gmail.com>
2023-11-20 20:28:11 +0100

    Sorting algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,8 +1,4 @@
 #include "sort.h"
-<<<<<<< HEAD
-#include <stdio.h>
-=======
->>>>>>> 3827af616c329c6cb1c6d9f2eb180e9b9fe2ee90
 
 /**
  * bubble_sort ->> Sorts an array of int in ascending the order.
@@ -12,38 +8,19 @@
 
 void bubble_sort(int *array, size_t size)
 {
-<<<<<<< HEAD
-    size_t i; c; s;
-
-    for (i = 0; i < size; i++)
-    {
-        for (c = 0; c <= size; i++)
-        {
-            if array[c - 1] > array[c]
-            {
-                s = array[c];
-                array[c] = array[c - 1];
-                array[j - 1] = s;
-                print_array(array, size);
-            }
-        }
-    }
-}
-=======
-	size_t i, c, s;
+	size_t i, j, s;
 
 	for (i = 0; i < size; i++)
 	{
-		for (c = 1; c < size - i; c++)
+		for (j = 1; j < size - i; j++)
 		{
-			if (array[c - 1] > array[c])
+			if (array[j - 1] > array[j])
 			{
-				s = array[c];
-				array[c] = array[c - 1];
-				array[c - 1] = s;
+				s = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = s;
 				print_array(array, size);
 			}
 		}
 	}
 }
->>>>>>> 3827af616c329c6cb1c6d9f2eb180e9b9fe2ee90
