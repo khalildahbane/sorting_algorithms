7bfcc2ad9a44a211a2ecfbe9337e5ecfa80f4d17
kelldani <kelldani100@gmail.com>
2023-11-22 13:04:43 +0100

    Sorting algorithms

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -17,5 +17,26 @@ void swap(int *a, int *b)
 
 void selection_sort(int *array, size_t size);
 {
-    for ()
+    for (size_t i = 0; i < size - 1; ++i)
+    {
+        size_t min_index = i;
+
+        for (size_t j = i + 1; j < size; ++j)
+        {
+            if (array[j] < array[min_index])
+            {
+                min_index = j;
+            }
+        }
+
+        if (min_index != i) {
+            swap(&array[i], &array[min_index]);
+            printf("Swap #%zu: ", i + 1);
+            for (size_t k = 0; k < size; ++k)
+            {
+                printf("%d ", array[k]);
+            }
+            printf("\n");
+        }
+    }
 }
