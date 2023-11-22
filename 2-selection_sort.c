723ced046e9c2005efb35baaea00ed8f57fbc150
kelldani <kelldani100@gmail.com>
2023-11-22 12:17:53 +0100

    Sorting algorithms

---
+++ 2-selection_sort.c
@@ -0,0 +1,21 @@
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+* selection_sort ->> Sorts an array of int in ascending the order.
+ * @array: Int Array.
+ * @size: matrixsize.
+*/
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selection_sort(int *array, size_t size);
+{
+    for ()
+}
