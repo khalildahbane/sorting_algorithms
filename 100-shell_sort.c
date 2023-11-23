b260cf9f93097e199a51013af0833ef07d343a51
kelldani <kelldani100@gmail.com>
2023-11-23 13:58:08 +0100

    Sorting algorithms

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,34 +8,34 @@
 
 void shell_sort(int *array, size_t size)
 {
-    size_t kun[1000], k = 0, j = 0, i;
-    int m;
+        size_t kun[1000], k = 0, j = 0, i;
+        int m;
 
-    if (!array)
-        return;
-    while (j * 3 + 1 < size)
-    {
-        kun[k] = j * 3 + 1;
-        j = kun[k++];
-    }
-    for (i = 0; i < k; i++)
-    {
-        for (j = 0; j < size; j++)
+        if (!array)
+            return;
+        while (j * 3 + 1 < size)
         {
-            if ((j + kun[k - i - 1]) > size - 1)
-                break;
-            k = j;
-            while (array[k] > array[k + kun[k - i - 1]])
-            {
-		int k;
-                m = array[k];
-                array[k] = array[k + kun[k - i - 1]];
-                array[k + kun[k - i - 1]] = m;
-                k = k - kun[k - i - 1];
-                if (k < 0)
-                    break;
-            }
+            kun[k] = j * 3 + 1;
+            j = kun[k++];
         }
-        print_array(array, size);
-    }
+        for (i = 0; i < k; i++)
+        {
+            for (j = 0; j < size; j++)
+            {
+                if ((j + kun[k - i - 1]) > size - 1)
+                    break;
+                k = j;
+                while (array[k] > array[k + kun[k - i - 1]])
+                {
+		    int k;
+                    m = array[k];
+                    array[k] = array[k + kun[k - i - 1]];
+                    array[k + kun[k - i - 1]] = m;
+                    k = k - kun[k - i - 1];
+                    if (k < 0)
+                        break;
+                }
+            }
+            print_array(array, size);
+        }
 }
---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,8 @@
+#include "sort.h"
+
+/**
+ * swap_plus ->> Swap nodes from left to right.
+ * @list: list.
+ * @_up: Node.
+ * @vary: auxiliary node.
+ */
